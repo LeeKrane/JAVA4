5a3efda9a4fcb65e4adc00dd58ffab1c8bb85eff
LeeKrane <godfist.leekrane@gmail.com>
2020-10-04 20:33:41 +0200

    Labor01: - updated N4

--- src/main/java/labor01/num4/EnglishWordFinder.java
+++ src/main/java/labor01/num4/EnglishWordFinder.java
@@ -1,32 +1,39 @@
 package labor01.num4;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class EnglishWordFinder {
 	public static void main (String[] args) {
 		Map<Integer, Character> letters = new TreeMap<>();
-		Set<String> words = getWordStream().collect(Collectors.toSet());
-		
-		getWordStream().forEach(word -> {
-			letters.clear();
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(EnglishWordFinder.class.getResourceAsStream("/labor01/words.txt")))) {
+			Set<String> words = reader.lines()
+					.map(String::toUpperCase)
+					.filter(word -> word.length() == 9 && word.chars().distinct().count() == 9)
+					.collect(Collectors.toSet());
 			
-			for (int i = 0; i < word.length(); i++) {
-				letters.put(i + 1, word.charAt(i));
+			for (String word : words) {
+				letters.clear();
+				
+				for (int i = 0; i < word.length(); i++) {
+					letters.put(i + 1, word.charAt(i));
+				}
+				
+				final String word2 = fetchWord(new int[] {2, 3, 1, 4, 5, 6, 7, 8, 9}, letters);
+				final String word3 = fetchWord(new int[] {8, 9, 3, 1, 2, 4, 5, 6, 7}, letters);
+				
+				if (words.contains(word) && words.contains(word2) && words.contains(word3)) {
+					System.out.println(word + '\n' + word2 + '\n' + word3 + '\n');
+				}
 			}
-			
-			final String word2 = fetchWord(new int[]{2, 3, 1, 4, 5, 6, 7, 8, 9}, letters);
-			final String word3 = fetchWord(new int[]{8, 9, 3, 1, 2, 4, 5, 6, 7}, letters);
-			
-			if (words.contains(word) && words.contains(word2) && words.contains(word3)) {
-				System.out.println(word + '\n' + word2 + '\n' + word3 + '\n');
-			}
-		});
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 	
 	private static String fetchWord (int[] pattern, Map<Integer, Character> letters) {
@@ -35,12 +42,5 @@ public class EnglishWordFinder {
 			builder.append(letters.get(i));
 		}
 		return builder.toString();
-	}
-	
-	private static Stream<String> getWordStream () {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(EnglishWordFinder.class.getResourceAsStream("/labor01/words.txt")));
-		return reader.lines()
-				.map(word -> word.toUpperCase())
-				.filter(word -> word.length() == 9 && word.chars().distinct().count() == 9);
 	}
 }
