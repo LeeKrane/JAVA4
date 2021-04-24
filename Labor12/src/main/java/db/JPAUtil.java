3815abe244d026ead2927b817545e3c3603b4610
LeeKrane <godfist.leekrane@gmail.com>
2021-04-24 21:56:23 +0200

    Updated Project Settings

--- Labor12/src/main/java/db/JPAUtil.java
+++ Labor12/src/main/java/db/JPAUtil.java
@@ -2,13 +2,22 @@ package db;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class JPAUtil {
 	private static EntityManagerFactory EMF = null;
 	
 	public static EntityManagerFactory getEMF() {
 		if(EMF == null) {
-			EMF = Persistence.createEntityManagerFactory("jpa-test-unit");
+			try (InputStream inputStream = JPAUtil.class.getResourceAsStream("/connection/connection.properties")) {
+				Properties properties = new Properties();
+				properties.load(inputStream);
+				EMF = Persistence.createEntityManagerFactory("jpa-test-unit", properties);
+			} catch (IOException e) {
+				System.err.println("IOException in JPAUtil Class: " + e.getMessage());
+			}
 		}
 		return EMF;
 	}
